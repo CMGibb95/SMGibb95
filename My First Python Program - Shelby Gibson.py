c28aed5945efbd227f502a82decc6399bc9293d9
Shelby Gibson <147758827+CMGibb95@users.noreply.github.com>
2024-09-25 08:39:01 -0400

    Add files via upload

---
+++ My First Python Program - Shelby Gibson.py
@@ -0,0 +1,79 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello World!")
+Hello World!
+print("Hello World!")
+Hello World!
+print("I just wrote my first Python Program")
+I just wrote my first Python Program
+print("This is so much fun")
+This is so much fun
+name = input("Hello what is your name?")
+Hello what is your name? Shelby
+print("Welcome it is so nice to meet you " + name)
+Welcome it is so nice to meet you  Shelby
+
+======== RESTART: /Users/tategibson/Documents/My First Python Program.py =======
+Traceback (most recent call last):
+  File "/Users/tategibson/Documents/My First Python Program.py", line 1, in <module>
+    Print("Hello World")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+
+print("Hello World")
+Hello World
+>>> 
+======== RESTART: /Users/tategibson/Documents/My First Python Program.py =======
+Traceback (most recent call last):
+  File "/Users/tategibson/Documents/My First Python Program.py", line 1, in <module>
+    Print("Hello World")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> 
+======== RESTART: /Users/tategibson/Documents/My First Python Program.py =======
+Hello World
+>>> 
+======== RESTART: /Users/tategibson/Documents/My First Python Program.py =======
+Hello World
+I just wrote my first Python Program
+This is so much fun
+>>> 
+======== RESTART: /Users/tategibson/Documents/My First Python Program.py =======
+Hello World
+I just wrote my first Python Program
+This is so much fun
+Hello what is your name?
+Hello what is your name? Shelby
+Welcome it is so nice to meet you  Shelby
+>>> 
+======== RESTART: /Users/tategibson/Documents/My First Python Program.py =======
+Hello World
+I just wrote my first Python Program
+This is so much fun
+Hello what is your name?
+Hello what is your name? 
+======== RESTART: /Users/tategibson/Documents/My First Python Program.py =======
+Hello World
+I just wrote my first Python Program
+This is so much fun
+Hello what is your name? Shelby
+Welcome it is so nice to meet you Shelby
+>>> 
+=============== RESTART: /Users/tategibson/Documents/My First Python Program.py ===============
+Hello World
+I just wrote my first Python Program
+This is so much fun
+Hello what is your name? 
+=============== RESTART: /Users/tategibson/Documents/My First Python Program.py ===============
+Hello World
+I just wrote my first Python Program
+This is so much fun
+Hello what is your name? 
+=============== RESTART: /Users/tategibson/Documents/My First Python Program.py ===============
+It's raining cats and dogs
+>>> 
+=============== RESTART: /Users/tategibson/Documents/My First Python Program.py ===============
+Hello World
+I just wrote my first Python Program
+This is so much fun
+Hello what is your name? Shelby
+Welcome it is so nice to meet you Shelby
+It's raining cats and dogs
