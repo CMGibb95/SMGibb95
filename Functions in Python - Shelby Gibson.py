421f3b94078ba4646d38bc2c074628821f6c8ed2
Shelby Gibson <147758827+CMGibb95@users.noreply.github.com>
2024-10-11 21:06:56 -0400

    Add files via upload

---
+++ Functions in Python - Shelby Gibson.py
@@ -0,0 +1,5 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: /Users/tategibson/Documents/Functions in Python - Shelby Gibson.py =
+The statement 2 is greater than 3 is false.
